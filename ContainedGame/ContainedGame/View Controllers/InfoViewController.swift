96162845f490a7735dd20605b18af58608a51564
blairholmgren <blair.holmgren@gmail.com>
2019-05-06 16:01:09 -0700

    Created crabs game

---
+++ ContainedGame/ContainedGame/View Controllers/InfoViewController.swift
@@ -0,0 +1,24 @@
+//
+//  InfoViewController.swift
+//  ContainedGame
+//
+//  Created by Blair Holmgren on 5/6/19.
+//  Copyright © 2019 Blair Holmgren. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+}
